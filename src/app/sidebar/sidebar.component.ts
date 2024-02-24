156d4b15715336aa2b60d5ce413adb9ca1d2944c
KSmayg0 <panferval@mail.ru>
2024-02-24 15:56:18 +0300

    Clear sidebar

--- src/app/sidebar/sidebar.component.ts
+++ src/app/sidebar/sidebar.component.ts
@@ -10,15 +10,18 @@ export interface RouteInfo {
 
 export const ROUTES: RouteInfo[] = [
     { path: '/request', title: 'Список заявок', icon:'nc-tile-56', class: ''},
-    { path: '/dashboard',     title: 'Dashboard',         icon:'nc-bank',       class: '' },
-    { path: '/icons',         title: 'Icons',             icon:'nc-diamond',    class: '' },
+    // { path: '/dashboard',     title: 'Dashboard',         icon:'nc-bank',       class: '' },
+    // { path: '/icons',         title: 'Icons',             icon:'nc-diamond',    class: '' },
     // { path: '/maps',          title: 'Maps',              icon:'nc-pin-3',      class: '' },
-    { path: '/notifications', title: 'Notifications',     icon:'nc-bell-55',    class: '' },
-    { path: '/user',          title: 'User Profile',      icon:'nc-single-02',  class: '' },
+    // { path: '/notifications', title: 'Notifications',     icon:'nc-bell-55',    class: '' },
     { path: '/table',         title: 'Сотрудники',        icon:'nc-badge',    class: '' },
     { path: '/brigade',    title: 'Бригады',        icon:'nc-user-run', class: '' },
+    { path: '/adress',    title: 'Рабочие машины',        icon:'nc-ambulance', class: '' },
         { path: '/adress',    title: 'Адреса',        icon:'nc-bullet-list-67', class: '' },
-    { path: '/upgrade',       title: 'Upgrade to PRO',    icon:'nc-spaceship',  class: 'active-pro' },
+    // { path: '/upgrade',       title: 'Upgrade to PRO',    icon:'nc-spaceship',  class: 'active-pro' },
+    { path: '/user',          title: 'Профиль',      icon:'nc-single-02',  class: '' },
+    { path: '/upgrade',    title: 'Выход',        icon:'nc-button-power', class: 'active-pro' },
+
 ];
 
 @Component({
